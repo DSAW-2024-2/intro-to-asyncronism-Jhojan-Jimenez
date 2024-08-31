6b1a3090218d66b036fbbe4adc8f88c7abc38018
Jhojan Jimenez <jhojanjimene@gmail.com>
2024-08-30 21:42:42 -0500

    Back Hecho

--- main.js
+++ main.js
@@ -3,21 +3,66 @@ const typesContainer = document.querySelector("#types");
 const leftButton = document.querySelector("#left");
 const rightButton = document.querySelector("#right");
 const inputName = document.querySelector("#search");
+const pokemonDetail = document.querySelector("#pokemon-details");
 let numPoke = 10;
-pokemones(1, 0, numPoke);
+let indexPoke = 1;
+pokemones(indexPoke, 0, numPoke);
 
 leftButton.addEventListener("click", () => {
   if (numPoke > 10) {
-    pokemones(1, numPoke-20, numPoke-10);
+    pokemones(indexPoke, numPoke - 20, numPoke - 10);
     numPoke = numPoke - 10;
   }
 });
 rightButton.addEventListener("click", () => {
-  pokemones(1, numPoke, numPoke + 10);
+  pokemones(indexPoke, numPoke, numPoke + 10);
   numPoke = numPoke + 10;
 });
-inputName.addEventListener("input", () => {
-  console.log(inputName.value);
+inputName.addEventListener("input", async () => {
+  let response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${inputName.value.toLowerCase()}`
+  );
+  if (response.status == 200 && inputName.value) {
+    pokemonsContainer.innerHTML = " ";
+    pokemonDetail.innerHTML = "";
+
+    const pokemon = await response.json();
+    pokemonDetail.classList.add("opacity");
+    const abilities = pokemon.abilities.map((abilitie) => {
+      return abilitie.ability.name;
+    });
+    inputName.blur();
+
+    pokemonDetail.innerHTML += `<div class="one-pokemon-container">
+                    <button class="back-button"><-</button>
+                    <img
+                        class="pokemon-img"
+                        src="${pokemon.sprites.other.dream_world.front_default}"
+                        alt=""
+                    />
+                    <div class="pokemon-info">
+                        <h2>${firstMayus(pokemon.name)}</h2>
+                        <p>Weight: ${pokemon.height} lb</p>
+                        <div class="pokemon-abilities">
+                            ${abilities
+                              .map((abilitie) => {
+                                return `<div class="abilitie">${firstMayus(
+                                  abilitie
+                                )}</div>`;
+                              })
+                              .join("")}
+                        </div>
+                    </div>
+    
+            </div>
+            `;
+    const backButton = document.querySelector(".back-button");
+    backButton.addEventListener("click", () => {
+      pokemonDetail.innerHTML = "";
+      pokemonDetail.classList.remove("opacity");
+      pokemones(indexPoke, 0, numPoke);
+    });
+  }
 });
 async function pokemones(indexT, off, limit) {
   try {
@@ -69,7 +114,7 @@ async function pokemones(indexT, off, limit) {
       button.textContent = firstMayus(type.name);
       button.addEventListener("click", () => {
         const index = typeIndex + 1;
-
+        indexPoke = index;
         pokemones(index, 0, 10);
       });
       typesContainer.appendChild(button);
